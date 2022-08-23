046d2980cd3887b5bc7d19ac178641bf4c87b5db
vmanz73 <firman.syahmaulana@yahoo.com>
2022-08-23 20:48:53 +0700

    vv

---
+++ Day 6/data_relational.sql
@@ -0,0 +1,5 @@
+INSERT INTO `operators` VALUES ('name_1', current_timestamp(), current_timestamp());
+INSERT INTO `operators` VALUES ('name_2', current_timestamp(), current_timestamp());
+INSERT INTO `operators` VALUES ('name_3', current_timestamp(), current_timestamp());
+INSERT INTO `operators` VALUES ('name_4', current_timestamp(), current_timestamp());
+INSERT INTO `operators` VALUES ('name_5', current_timestamp(), current_timestamp());
