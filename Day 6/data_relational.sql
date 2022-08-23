1f62fffe229a486d732177dda9fc6415fcdc6c1e
vmanz73 <firman.syahmaulana@yahoo.com>
2022-08-23 21:00:25 +0700

    product_description

--- Day 6/data_relational.sql
+++ Day 6/data_relational.sql
@@ -20,3 +20,13 @@ INSERT INTO `products` VALUES (2, 1, "product_5", "meja", 1, current_timestamp()
 INSERT INTO `products` VALUES (3, 4, "product_6", "baju", 1, current_timestamp(), current_timestamp());
 INSERT INTO `products` VALUES (3, 4, "product_7", "celana", 1, current_timestamp(), current_timestamp());
 INSERT INTO `products` VALUES (3, 4, "product_8", "jaket", 1, current_timestamp(), current_timestamp());
+
+
+INSERT INTO `product_description` VALUES (1, "ini adalah kulkas", current_timestamp(), current_timestamp());
+INSERT INTO `product_description` VALUES (2, "ini adalah tv", current_timestamp(), current_timestamp());
+INSERT INTO `product_description` VALUES (3, "ini adalah lemari", current_timestamp(), current_timestamp());
+INSERT INTO `product_description` VALUES (4, "ini adalah kursi", current_timestamp(), current_timestamp());
+INSERT INTO `product_description` VALUES (5, "ini adalah meja", current_timestamp(), current_timestamp());
+INSERT INTO `product_description` VALUES (6, "ini adalah baju", current_timestamp(), current_timestamp());
+INSERT INTO `product_description` VALUES (7, "ini adalah celana", current_timestamp(), current_timestamp());
+INSERT INTO `product_description` VALUES (8, "ini adalah jaket", current_timestamp(), current_timestamp());
