98715ee65f94888caeeea4aa694317637354a791
vmanz73 <firman.syahmaulana@yahoo.com>
2022-08-23 20:51:01 +0700

    product_types

--- Day 6/data_relational.sql
+++ Day 6/data_relational.sql
@@ -1,5 +1,13 @@
+
 INSERT INTO `operators` VALUES ('name_1', current_timestamp(), current_timestamp());
 INSERT INTO `operators` VALUES ('name_2', current_timestamp(), current_timestamp());
 INSERT INTO `operators` VALUES ('name_3', current_timestamp(), current_timestamp());
 INSERT INTO `operators` VALUES ('name_4', current_timestamp(), current_timestamp());
 INSERT INTO `operators` VALUES ('name_5', current_timestamp(), current_timestamp());
+
+INSERT INTO `product_types` VALUES ('type_1', current_timestamp(), current_timestamp());
+INSERT INTO `product_types` VALUES ('type_2', current_timestamp(), current_timestamp());
+INSERT INTO `product_types` VALUES ('type_3', current_timestamp(), current_timestamp());
+INSERT INTO `product_types` VALUES ('type_4', current_timestamp(), current_timestamp());
+INSERT INTO `product_types` VALUES ('type_5', current_timestamp(), current_timestamp());
+
