8ea4f0b562d8d8b63c2b83369d6574bef376875b
vmanz73 <firman.syahmaulana@yahoo.com>
2022-08-23 20:57:16 +0700

    products

--- Day 6/data_relational.sql
+++ Day 6/data_relational.sql
@@ -11,3 +11,12 @@ INSERT INTO `product_types` VALUES ('type_3', current_timestamp(), current_times
 INSERT INTO `product_types` VALUES ('type_4', current_timestamp(), current_timestamp());
 INSERT INTO `product_types` VALUES ('type_5', current_timestamp(), current_timestamp());
 
+
+INSERT INTO `products` VALUES (1, 3, "product_1", "kulkas", 1, current_timestamp(), current_timestamp());
+INSERT INTO `products` VALUES (1, 3, "product_2", "tv", 1, current_timestamp(), current_timestamp());
+INSERT INTO `products` VALUES (2, 1, "product_3", "lemari", 1, current_timestamp(), current_timestamp());
+INSERT INTO `products` VALUES (2, 1, "product_4", "kursi", 1, current_timestamp(), current_timestamp());
+INSERT INTO `products` VALUES (2, 1, "product_5", "meja", 1, current_timestamp(), current_timestamp());
+INSERT INTO `products` VALUES (3, 4, "product_6", "baju", 1, current_timestamp(), current_timestamp());
+INSERT INTO `products` VALUES (3, 4, "product_7", "celana", 1, current_timestamp(), current_timestamp());
+INSERT INTO `products` VALUES (3, 4, "product_8", "jaket", 1, current_timestamp(), current_timestamp());
