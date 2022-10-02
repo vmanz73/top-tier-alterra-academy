02ca2e1e03643f0c1ca53d16dc7ae869dcc80463
vmanz73 <firman.syahmaulana@yahoo.com>
2022-10-03 02:32:15 +0700

    routing navbar

--- FE/Day 6-7/task-alttera/src/app/app-routing.module.ts
+++ FE/Day 6-7/task-alttera/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { MainFormComponent } from './main-form/main-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'passenger', component: MainFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
